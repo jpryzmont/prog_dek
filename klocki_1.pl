36554401bd3bf4185a6c55f64ad43d767970329c
jpryzmont <pryzmontkuba@gmail.com>
2026-02-25 12:46:34 +0100

    Add files via upload

---
+++ klocki_1.pl
@@ -0,0 +1,21 @@
+% PROGRAM: klocki_1
+% Baza wiedzy o układzie klocków
+% Definiowane predykaty:
+% 	na/2
+%==================================
+% na(X,Y)
+% opis: spełniony, gdy klocek X leży
+% bezposrednio na klocku Y
+%----------------------------------
+	na(c,a).
+	na(c,b).
+	na(d,c).
+%----------------------------------
+/*
+Inomrajce o budowie programu:
+Program składa sie z 3 klauzul.
+Program zawiera 1 defincje relacji.
+Jest to relacaj na/2.
+Defincja relacji na/2 składa sie z
+3 klazul, które sa faktami.
+*/ 
