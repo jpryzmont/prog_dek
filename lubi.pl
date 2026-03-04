c95f9e78a25572a9773d5cf11cdaeb4f36dd5fd5
jpryzmont <pryzmontkuba@gmail.com>
2026-03-04 12:23:01 +0100

    Update lubi.pl

--- lubi.pl
+++ lubi.pl
@@ -10,3 +10,46 @@ program składa się z 7 klauzul:
 6 faktow (lubi) i 1 regula (bratnia dusza)
 2 definicje relacji (lubi, bratnia dusza)
 */
+
+
+/*
+Test:
+1.Czy Jerzy lubi sudety?
+?- lubi(jerzy,sudety).
+false.
+
+2.Co lubi Jan?
+?- lubi(jan,X).
+X = tatry ;
+X = beskidy.
+
+3.Kto lubi bieszczady?
+?- lubi(X,bieszczady).
+X = jerzy ;
+X = justyna.
+
+4.Czy Jan ma bratnia dusze?
+?- bratnia_dusza(jan,_).
+true.
+
+5.Kto jest bratnia dusza Justyny?
+?- bratnia_dusza(justyna,X).
+X = jerzy ;
+
+6.Jakie pary osob ssa bratnimi duszami?
+?- bratnia_dusza(Y,X).
+Y = jan,
+X = jerzy ;
+Y = jerzy,
+X = jan ;
+Y = jerzy,
+X = justyna ;
+Y = justyna,
+X = jerzy ;
+false.
+
+7.Jakie gory jednoczesnie lubi Jan i Jerzy? (, to koniunkcja)
+?- lubi(jan,X),lubi(jerzy,X).
+X = beskidy ;
+
+*/
