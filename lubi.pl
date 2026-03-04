81263900402e3adcba5cce7867980919fe23b9c2
jpryzmont <pryzmontkuba@gmail.com>
2026-03-04 12:10:13 +0100

    Update lubi.pl

--- lubi.pl
+++ lubi.pl
@@ -5,3 +5,8 @@ lubi(jerzy,bieszczady).
 lubi(adam,sudety).
 lubi(justyna,bieszczady).
 bratnia_dusza(X,Y):-lubi(X,G),lubi(Y,G),X\==Y.
+/*
+program składa się z 7 klauzul:
+6 faktow (lubi) i 1 regula (bratnia dusza)
+2 definicje relacji (lubi, bratnia dusza)
+*/
