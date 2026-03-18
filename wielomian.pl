692fec2a513c0e5fc356f07a4159cf85d733f105
jpryzmont <pryzmontkuba@gmail.com>
2026-03-18 12:53:55 +0100

    Update wielomian.pl

--- wielomian.pl
+++ wielomian.pl
@@ -20,5 +20,5 @@ Definicja: rekurencja strukturalna
 	wielomian(-W,X):- wielomian(W,X).
 	wielomian(W1+W2,X:- wielomian(W1,X), wielomian(W2,X).
 	wielomian(W1-W2,X):- wielomian(W1,X), wielomian(W2,X).
-	wielomian(W1*W2,X):- wielomian(W1,X), wielomian(W1,X).
+	wielomian(W1*W2,X):- wielomian(W1,X), wielomian(W2,X).
 	wielomian(W^N,X):- integer(N), N>1, wielomian(W,X).
